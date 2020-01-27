1854b7795796b06855acfeda79bac14f1e281781
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-01-27 23:41:25 +0100

    Filter out inlined

--- model-lowerings/src/org/jetbrains/dukat/commonLowerings/extractNonExternalDeclarations.kt
+++ model-lowerings/src/org/jetbrains/dukat/commonLowerings/extractNonExternalDeclarations.kt
@@ -60,7 +60,7 @@ private fun ModuleModel.canNotContainExternalEntities(): Boolean {
 private fun ModuleModel.filterOutExternalDeclarations(): ModuleModel {
     return if (canNotContainExternalEntities()) {
         copy(
-                declarations = declarations.filterNot { (it is TypeAliasModel) },
+                declarations = declarations.filterNot { (it is TypeAliasModel) || ((it is FunctionModel) && it.inline) },
                 submodules = submodules.map { it.filterOutExternalDeclarations() }
         )
     } else {
@@ -83,7 +83,7 @@ fun SourceSetModel.extractNonExternalDeclarations(): SourceSetModel {
     }
 
     val sourcesLowered =
-            generateDeclarationFiles("aliases", aliasBucket) + sources.map { source -> source.copy(root = source.root.filterOutExternalDeclarations()) }
+            generateDeclarationFiles("aliases", aliasBucket) + generateDeclarationFiles("inlined", functionsBucket) + sources.map { source -> source.copy(root = source.root.filterOutExternalDeclarations()) }
 
     return copy(sources = sourcesLowered)
 }
