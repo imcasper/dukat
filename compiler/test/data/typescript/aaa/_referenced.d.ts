df7b6e0d44fc89cfe20cd5b742f3fbb58b04a0c9
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-01-27 19:51:06 +0100

    TO REVERT remove breaking case

--- compiler/test/data/typescript/aaa/_referenced.d.ts
+++
@@ -1,6 +0,0 @@
-
-declare module "api" {
-    interface SomeAPI {
-        pong(): boolean
-    }
-}
--- compiler/test/data/typescript/aaa/aaa.d.ts
+++
@@ -1,8 +0,0 @@
-
-/// <reference path="_referenced.d.ts"/>
-
-declare module "api" {
-    interface SomeAPI {
-        ping(): boolean;
-    }
-}
